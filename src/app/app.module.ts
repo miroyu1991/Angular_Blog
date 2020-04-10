2902eb580e9cf1de866cc49d256ac2ab6e44b49f
miroyu1991 <46637440+miroyu1991@users.noreply.github.com>
2020-04-10 17:08:49 -0400

    code

---
+++ src/app/app.module.ts
@@ -0,0 +1,56 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { BlogComponent } from './blog/blog.component';
+import { PostComponent } from './post/post.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { PostCardComponent } from './post-card/post-card.component';
+import { SearchWidgetComponent } from './search-widget/search-widget.component';
+import { LatestPostComponent } from './latest-post/latest-post.component';
+import { CategoriesComponent } from './categories/categories.component';
+import { TagsComponent } from './tags/tags.component';
+import { PostDataComponent } from './post-data/post-data.component';
+import { PagingComponent } from './paging/paging.component';
+import { FooterPostsComponent } from './footer-posts/footer-posts.component';
+import { EditPostComponent } from './edit-post/edit-post.component';
+import { NewPostComponent } from './new-post/new-post.component';
+import { PostsTableComponent } from './posts-table/posts-table.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    BlogComponent,
+    PostComponent,
+    PageNotFoundComponent,
+    PostCardComponent,
+    SearchWidgetComponent,
+    LatestPostComponent,
+    CategoriesComponent,
+    TagsComponent,
+    PostDataComponent,
+    PagingComponent,
+    FooterPostsComponent,
+    EditPostComponent,
+    NewPostComponent,
+    PostsTableComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
